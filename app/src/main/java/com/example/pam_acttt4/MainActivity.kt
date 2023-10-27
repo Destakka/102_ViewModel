8a4efe4a67a81270038749d8accd0d061dbc9eed
DESTA ATIKANANDA <d.atikananda.ft21@mail.umy.ac.id>
2023-10-27 17:31:57 +0700

    add icon n text

--- app/src/main/java/com/example/pam_acttt4/MainActivity.kt
+++ app/src/main/java/com/example/pam_acttt4/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.pam_acttt4
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -11,6 +12,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.selection.selectable
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
@@ -33,8 +35,11 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -58,7 +63,6 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
@@ -68,19 +72,35 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 }
 
 @Composable
-fun TampilLayout(modifier: Modifier = Modifier) {
-    Card (
+fun TampilLayout(
+modifier: Modifier = Modifier) {
+    Card(
         modifier = Modifier,
         elevation = CardDefaults.cardElevation(defaultElevation = 5.dp)
     ) {
+        Row (verticalAlignment = Alignment.CenterVertically) {
+            Image(
+            painter = painterResource(
+                id = R.drawable.baseline_arrow_back_24
+            ),
+                contentDescription = "",
+                modifier = Modifier.size(20.dp)
+            )
+            Text(
+                text = "Register",
+                fontSize = 15.sp,
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Center,
+                modifier = Modifier.padding(horizontal = 150.dp, vertical = 10.dp)
+            )}
         Column(
             verticalArrangement = Arrangement.spacedBy(10.dp),
             horizontalAlignment = Alignment.CenterHorizontally,
             modifier = Modifier.padding(20.dp)
-        ) {
+        )
+        {
             TampilForm()
         }
-
     }
 }
 
